fafe9e587aac8ba86cefbc836f2e18b3dd28d89a
Kamireddi, Gaurav Venkata Satya Pratik <kamireddig2370@d158b-44276.uhcl.edu>
2017-11-22 13:22:06 -0600

    Initial Commit

---
+++ Number Conversions Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Number Conversions Swift
+//
+//  Created by Kamireddi, Gaurav Venkata Satya Pratik on 11/22/17.
+//  Copyright © 2017 Kamireddi, Gaurav Venkata Satya Pratik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Number Conversions SwiftTests/Number_Conversions_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Number_Conversions_SwiftTests.swift
+//  Number Conversions SwiftTests
+//
+//  Created by Kamireddi, Gaurav Venkata Satya Pratik on 11/22/17.
+//  Copyright © 2017 Kamireddi, Gaurav Venkata Satya Pratik. All rights reserved.
+//
+
+import XCTest
+@testable import Number_Conversions_Swift
+
+class Number_Conversions_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
